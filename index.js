fc6494c62c169bbe4c6731c9d58e837383e66fd8
MIHIRANGA S.S <sachinmihiranga2000@gmail.com>
2024-06-06 11:31:51 +0530

    CRUD operations & JWT verification

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import hotelsRoute from "./api/routes/hotels.js"
 import roomsRoute from "./api/routes/rooms.js"
 import usersRoute from "./api/routes/users.js"
 import authRoute from "./api/routes/auth.js"
+import cookieParser from "cookie-parser"
 const app = express()
 dotenv.config()
 
@@ -28,13 +29,24 @@ mongoose.connection.on("disconnected", ()=>{
 // app.get("/users",(req,res)=>{
 //     res.send("hello first request!")
 // })
-
+app.use(cookieParser())
 app.use(express.json())
 
 app.use("/api/auth",authRoute);
 app.use("/api/hotels",hotelsRoute);
 app.use("/api/rooms",roomsRoute);
-app.use("/api/users",usersRoute);
+app.use("/api/users",usersRoute); 
+
+app.use((err, req, res, next) => {
+  const errorStatus = err.status || 500;
+  const errorMessage = err.message || "Something went wrong!";
+  return res.status(errorStatus).json({
+    success: false,
+    status: errorStatus,
+    message: errorMessage,
+    stack: err.stack,
+  });
+});
 
 
 app.listen(8800,()=>{
